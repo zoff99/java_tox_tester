0b0edb4bcdbde6f741f76405e73577849c3a025e
zoff99 <zoff99@users.noreply.github.com>
2024-04-26 14:36:46 +0200

    compile classes and add to compile script

--- do_compile.sh
+++ do_compile.sh
@@ -5,3 +5,4 @@ javac com/zoffcc/applications/trifa/TRIFAGlobals.java
 javac com/zoffcc/applications/trifa/ByteBufferCompat.java
 javac com/zoffcc/applications/trifa/MainActivity.java
 javac com/zoffcc/applications/trifa/TrifaToxService.java
+javac com/zoffcc/applications/trifa/OperatingSystem.java
